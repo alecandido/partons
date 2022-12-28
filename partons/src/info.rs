fdbb05a88f71d2aca4b8a4f984da3f52bc442c15
Alessandro Candido <candido.ale@gmail.com>
2022-12-28 19:53:04 +0100

    Remove all case related warnings

--- partons/src/info.rs
+++ partons/src/info.rs
@@ -6,56 +6,68 @@ pub type PID = String;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Info {
-    SetDesc: String,
-    SetIndex: u32,
-    Authors: String, // TODO: replace with sequence of strings
-    #[serde(default)]
-    Reference: Option<String>,
-    #[serde(default)]
-    Format: Option<String>, // TODO: replace with enum
-    DataVersion: u32,
-    NumMembers: u32,
-    #[serde(default)]
-    Particle: Option<PID>,
-    Flavors: Vec<PID>,
-    OrderQCD: u32,
-    FlavorScheme: String, // TODO: replace with enum
-    #[serde(default)]
-    NumFlavors: Option<u32>,
-    #[serde(default)]
-    ErrorType: Option<String>, // TODO: replace with enum
-    XMin: f64,
-    XMax: f64,
-    QMin: f64,
-    QMax: f64,
-    #[serde(default)]
-    MZ: Option<f64>,
-    #[serde(default)]
-    MUp: Option<f64>,
-    #[serde(default)]
-    MDown: Option<f64>,
-    #[serde(default)]
-    MStrange: Option<f64>,
-    #[serde(default)]
-    MCharm: Option<f64>,
-    #[serde(default)]
-    MBottom: Option<f64>,
-    #[serde(default)]
-    MTop: Option<f64>,
-    #[serde(default)]
-    AlphaS_MZ: Option<f64>,
-    #[serde(default)]
-    AlphaS_OrderQCD: Option<u32>,
-    #[serde(default)]
-    AlphaS_Type: Option<String>, // TODO: replace with enum
-    #[serde(default)]
-    AlphaS_Qs: Option<Vec<f64>>,
-    #[serde(default)]
-    AlphaS_Vals: Option<Vec<f64>>,
-    #[serde(default)]
-    AlphaS_Lambda4: Option<f64>,
-    #[serde(default)]
-    AlphaS_Lambda5: Option<f64>,
-    #[serde(default)]
-    Extrapolator: Option<String>, // TODO: replace with enum
+    #[serde(rename = "SetDesc")]
+    set_desc: String,
+    #[serde(rename = "SetIndex")]
+    set_index: u32,
+    #[serde(rename = "Authors")]
+    authors: String, // TODO: replace with sequence of strings
+    #[serde(default, rename = "Reference")]
+    reference: Option<String>,
+    #[serde(default, rename = "Format")]
+    format: Option<String>, // TODO: replace with enum
+    #[serde(rename = "DataVersion")]
+    data_version: u32,
+    #[serde(rename = "NumMembers")]
+    num_members: u32,
+    #[serde(default, rename = "Particle")]
+    particle: Option<PID>,
+    #[serde(rename = "Flavors")]
+    flavors: Vec<PID>,
+    #[serde(default, rename = "OrderQCD")]
+    order_qcd: Option<u32>,
+    #[serde(rename = "FlavorScheme")]
+    flavor_scheme: String, // TODO: replace with enum
+    #[serde(default, rename = "NumFlavors")]
+    num_flavors: Option<u32>,
+    #[serde(default, rename = "ErrorType")]
+    error_type: Option<String>, // TODO: replace with enum
+    #[serde(rename = "XMin")]
+    x_min: f64,
+    #[serde(rename = "XMax")]
+    x_max: f64,
+    #[serde(rename = "QMin")]
+    q_min: f64,
+    #[serde(rename = "QMax")]
+    q_max: f64,
+    #[serde(default, rename = "MZ")]
+    mz: Option<f64>,
+    #[serde(default, rename = "MUp")]
+    m_up: Option<f64>,
+    #[serde(default, rename = "MDown")]
+    m_down: Option<f64>,
+    #[serde(default, rename = "MStrange")]
+    m_strange: Option<f64>,
+    #[serde(default, rename = "MCharm")]
+    m_charm: Option<f64>,
+    #[serde(default, rename = "MBottom")]
+    m_bottom: Option<f64>,
+    #[serde(default, rename = "MTop")]
+    m_top: Option<f64>,
+    #[serde(default, rename = "AlphaS_MZ")]
+    alpha_s_mz: Option<f64>,
+    #[serde(default, rename = "AlphaS_OrderQCD")]
+    alpha_s_order_qcd: Option<u32>,
+    #[serde(default, rename = "AlphaS_Type")]
+    alpha_s_type: Option<String>, // TODO: replace with enum
+    #[serde(default, rename = "AlphaS_Qs")]
+    alpha_s_qs: Option<Vec<f64>>,
+    #[serde(default, rename = "AlphaS_Vals")]
+    alpha_s_vals: Option<Vec<f64>>,
+    #[serde(default, rename = "AlphaS_Lambda4")]
+    alpha_s_lambda4: Option<f64>,
+    #[serde(default, rename = "AlphaS_Lambda5")]
+    alpha_s_lambda5: Option<f64>,
+    #[serde(default, rename = "Extrapolator")]
+    extrapolator: Option<String>, // TODO: replace with enum
 }
