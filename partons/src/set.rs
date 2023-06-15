c6ce3e7c9121faf294713adb1e78306eea54e526
Alessandro Candido <candido.ale@gmail.com>
2023-06-15 16:55:39 +0200

    Provide docs stubs

--- partons/src/set.rs
+++ partons/src/set.rs
@@ -10,20 +10,26 @@ use crate::{
     member::Member,
 };
 
+/// Partons set.
 pub struct Set {
     source: Source,
     path: PathBuf,
 }
 
 impl Set {
+    /// Set name.
+    ///
+    /// Usually unique enough to identify the set within a source.
     pub fn name(&self) -> String {
         self.path.file_name().unwrap().to_str().unwrap().to_owned()
     }
 
+    /// Original source from which the data have been fetched.
     pub fn source(&self) -> String {
         self.source.name.clone()
     }
 
+    /// Metadata.
     pub fn info(&self) -> Result<Info> {
         let relative = Resource::Info(self.name()).path();
         let mut path = self.path.clone();
@@ -32,6 +38,7 @@ impl Set {
         Info::load(fs::read(path)?.into())
     }
 
+    /// Retrieve a set member.
     pub fn member(&self, num: u32) -> Result<Member> {
         let relative = Resource::Member(self.name(), num).path();
         let mut path = self.path.clone();
